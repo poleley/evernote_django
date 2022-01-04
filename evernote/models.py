31fa017c3f76160a922d1263deb56adb913a6a7c
Polina Polovnikova <polli07@mail.ru>
2022-01-05 00:57:07 +0700

    some fixes

--- evernote/models.py
+++ evernote/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Note(models.Model):
+    person = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=45, null=False)
+    date = models.DateField(auto_now=True, null=False)
+    text = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=45, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class NoteHasTag(models.Model):
+    note = models.ForeignKey(Note, on_delete=models.CASCADE, null=False)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, null=False)
--- evernote/views.py
+++ evernote/views.py
@@ -1,16 +1,22 @@
 from django.template.loader import render_to_string
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+
 
 def show_main(request):
-    responce = render_to_string('evernote/main.html')
-    return HttpResponse(responce)
+    notes = Note.objects.all()
+    tags = Tag.objects.all()
+    note_has_tag = NoteHasTag.objects.all()
+    response = render(request, 'evernote/main.html', {'notes': notes, 'tags': tags, 'note_has_tag' : note_has_tag})
+    return HttpResponse(response)
 
 
 def landing(request):
-    responce = render_to_string('evernote/index.html')
-    return HttpResponse(responce)
+    response = render_to_string('evernote/index.html')
+    return HttpResponse(response)
 
 
 def registration(request):
-    responce = render_to_string('evernote/registration.html')
-    return HttpResponse(responce)
+    response = render_to_string('evernote/registration.html')
+    return HttpResponse(response)
